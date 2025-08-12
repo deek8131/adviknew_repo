5ad6a71222829be9a8efc62eadd62831ac39d186
Deeksha <deeksha@gmail.com>
2025-08-12 18:18:52 +0530

    test

--- Env/Dev/main.tf
+++ Env/Dev/main.tf
@@ -9,3 +9,25 @@ resource "azurerm_resource_group" "example" {
     name     = "example-resources"
     location = "East US"
 }
+
+resource "azapi_resource" "arcEnabledMachine" {
+  type      = "Microsoft.HybridCompute/machines@2021-05-20"
+  name      = "name"
+  location  = "location"
+}
+
+resource "azapi_resource" "guestConfigAssignment" {
+  type      = "Microsoft.GuestConfiguration/guestConfigurationAssignments@2022-01-25"
+  name      = "name"
+  parent_id = azapi_resource.arcEnabledMachine.id
+  location  = "location"
+  body = {
+    properties = {
+      guestConfiguration = {
+        name            = "configurationName"
+        assignmentType  = "ApplyAndMonitor"
+        version         = "1.*"
+      }
+    }
+  }
+}
